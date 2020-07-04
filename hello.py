694b6106e70d1424c2490f8e1da712268a5291f6
Moein <moein.shariatnia@gmail.com>
2020-07-04 14:23:16 +0430

    some added

--- hello.py
+++ hello.py
@@ -1,20 +1,6 @@
-import math
-import sys
-from os import rename
-
 import requests
 
-# print(sys.version)
-print("hello")
-print(sys.executable)
-
-
-def func(arg1, arg2):
-    # var = "something"
-    return arg1 * arg2
-
-
-# print("hello world")
 r = requests.get("http://sicoms.surge.sh/")
 print(r.status_code)
+print(r.ok)
 # "python.pythonPath": "C:\\Users\\sadra\\.conda\\envs\\pytorch-env\\python.exe",
